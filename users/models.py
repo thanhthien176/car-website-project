c2e5f0f83102dd277651b7c83e39a4ebd60d3fb0
thiennt176 <thanhthien176@gmail.com>
2026-04-01 15:38:51 +0700

    feat(users): implement UserProfile and SavedCar models in users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+    avatar = models.ImageField(upload_to="users/avatars/", blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null= True)
+    city = models.CharField(max_length=100, blank=True)
+    bio = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = 'Hồ Sơ Người Dùng'
+        verbose_name_plural = 'Hồ Sơ Người Dùng'
+        
+    def __str__(self):
+        return f"Profile: {self.user.username}"    
+
+class SavedCar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='saved_cars')
+    car = models.ForeignKey('cars.Car', on_delete=models.CASCADE, related_name='saved_by')
+    saved_at = models.DateTimeField(auto_now_add=True)
+    note = models.TextField(blank=True)
+    
+    class Meta:
+        unique_together = ['user', 'car']
+        ordering = ['saved_at']
+        verbose_name = "Xe đã lưu"
+        
+    def __str__(self):
+        return f"{self.user.username} -> {self.car.name}"
+    
+    
+    
