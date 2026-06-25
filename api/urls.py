834865b24dd78e7b0a404970d4d23e10e312281d
thiennt176 <thanhthien176@gmail.com>
2026-06-26 06:47:30 +0700

    rename: rename cars to car_api and update car_api in api.urls

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 app_name = 'api'
 
 urlpatterns = [
-    path('', include('api.cars.urls')),
+    path('', include('api.car_api.urls')),
     # path('users/', include('api.users.urls'))
 ]
